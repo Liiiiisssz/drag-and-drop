ae7852c370435f57128ba46c9e10d0d68dd6d8e9
Lis <elis_jasper@estudante.sesisenai.org.br>
2025-11-07 20:54:03 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const draggable = document.querySelector('.draggable')
+const draggables = document.querySelectorAll('.draggable')
 const dropzone = document.querySelector('.dropzone')
-const originalParent = draggable.parentElement
+
+const texto = document.querySelector('.texto')
+
+let dragged = null
+
+draggables.forEach(item =>{
+    item.setAttribute('draggable', 'true')
+
+    const originalParent = item.parentElement
+
+    item.addEventListener('dragstart', (event) =>{
+        dragged = event.currentTarget
+        dropzone.classList.add('possible-drop')
+    })
+
+    item.addEventListener('click', function(){
+        originalParent.appendChild(item)
+    })
+})
 
 dropzone.addEventListener('dragenter', (event) =>{
     event.preventDefault()
@@ -11,11 +29,15 @@ dropzone.addEventListener('dragover', (event) =>{
     event.preventDefault()
 })
 
-dropzone.addEventListener('drop', (event) =>{
-    event.preventDefault()
-    dropzone.appendChild(draggable)
+dropzone.addEventListener('dragleave', ()=>{
+    dropzone.classList.remove('over')
 })
 
-draggable.addEventListener('click', function(){
-    originalParent.appendChild(draggable)
+dropzone.addEventListener('drop', (event) =>{
+    event.preventDefault()
+    texto.textContent = ""
+    dropzone.classList.remove('over')
+    if(dragged){
+        dropzone.appendChild(dragged)
+    }
 })
